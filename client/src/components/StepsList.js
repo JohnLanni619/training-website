39a0fbf2a79df053598eafe8a56f6768041d342d
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-10-13 20:56:32 -0400

    built stepsList component

---
+++ client/src/components/StepsList.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { RiNumber1, RiNumber2, RiNumber3, RiNumber4} from "react-icons/ri"
+
+const steps = [
+    {
+        number: <RiNumber1/>,
+        title: 'Fill Out Contact Form',
+        description: 'We will reach out to you to schedule a consultation'
+    },
+    {
+        number: <RiNumber2/>,
+        title: 'The Consultation',
+        description: "During your consultation, our certified personal trainers will talk to you about your goals and get a real understanding of what you want to accomplish. We will also do a movement assesment to ensure that the program we're writing for you will maximize your results."
+    },
+    {
+        number: <RiNumber3/>,
+        title: 'The Plan',
+        description: "After the consultation, you and your trainer will determine which path will maximize your results. Your roadmap will be broken down into a weekly routine to take the guesswork out of fitness."
+    },
+    {
+        number: <RiNumber4/>,
+        title: 'The Journey',
+        description: "You'll be following your plan and will be receiving support from your coach every step of the way. Our coaches will check in with you weekly or even daily depending on your needs, and will be there to help you problem solve and be a source of encouragement along the way."
+    }
+];
+
+const StepsList = () => {
+    return(
+        <section>
+            {steps.map((step) => {
+                return(
+                    <div key={step.title}>
+                        <span>{step.number}</span>
+                        <h3>{step.title}</h3>
+                        <p>{step.description}</p>
+                    </div>
+                )
+            })}
+        </section>
+    )
+};
+
+export default StepsList;
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import headshot from '../../assets/csheadshot.jpg';
 import Mission from '../Mission';
 import TestimonialsList from "../TestimonialsList";
+import StepsList from "../StepsList";
 
 const About = () => {
     return(
@@ -14,6 +15,7 @@ const About = () => {
                    <p className='about-item'>I'm an aspiring full stack web developer. I just finished the Rutgers Coding Bootcamp in September. I want to develop applications that both solve problems and enhance the user's experience. I'd like to work in an environment that facilitates continuous learning and development. As a former personal training manager, I'm proficient in working with a team towards a common goal. When I'm not coding, I'm working as a personal trainer and spending time with my three year old son.</p>
                 </div>
             </section>
+            <StepsList/>
             <TestimonialsList/>
         </main>
     )
